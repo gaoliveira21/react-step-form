e6cc5a8e098e9e7056c44176b3a6d807220bcd46
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-19 14:47:52 -0300

    feat: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React, { ReactElement } from 'react'
 
 import { Steps } from '@/components/Steps'
 
+import css from './App.sass'
+
 export const App: React.FC = (): ReactElement => {
   return (
-    <Steps components={[
-      <p key="1">step 1</p>,
-      <p key="1">step 2</p>,
-      <p key="1">step 3</p>,
-      <p key="1">step 4</p>
-    ]} />
+    <div className={css.App}>
+      <Steps components={[
+        <p key="1">step 1</p>,
+        <p key="1">step 2</p>,
+        <p key="1">step 3</p>,
+        <p key="1">step 4</p>
+      ]} />
+    </div>
   )
 }
--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement, useState } from 'react'
 
 import { Indicator } from '../Indicator'
 
+import css from './Steps.sass'
+
 type ISteps = {
   components: ReactElement[]
 }
@@ -10,10 +12,12 @@ export const Steps: React.FC<ISteps> = ({ components }): ReactElement => {
   const [currentStep, setCurrentStep] = useState(0)
 
   return (
-    <div>
-      <h2>steps</h2>
+    <div className={css.Steps}>
+      <header className={css.S__Header}>
+        <h2>Formulário de cadastro</h2>
+      </header>
       {components[currentStep]}
-      <div>
+      <div className={css.S__Indicators}>
         {components.map((_, index) => (
           <Indicator
             key={index}
